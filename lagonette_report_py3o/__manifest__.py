185e525b96a626650263cb15456bf4f93e6983e1
Laetitia Da Costa <laetitia.dacosta@elabore.coop>
2024-02-13 15:19:43 +0100

    [ADD]lagonette py2o report

---
+++ lagonette_report_py3o/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'La Gonette Reports Py3o',
+    'version': '12.0.1.0.0',
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    'summary': 'La Gonette py3o reports',
+    'description': """
+La Gonette Reports Py3o
+=================
+
+    """,
+    'author': 'Elabore',
+    'depends': [
+        'report_py3o',
+        ],
+    'data': ['config.xml','report.xml'],
+    'installable': True,
+}
